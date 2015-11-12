abc7774b85255dd16bd480d6e17c8b989b4eb05c
Josh Austin <josh.austin@gmail.com>
2015-11-12 10:24:21 -0500

    update version requirements to be more flexible

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExCrypto.Mixfile do
     [app: :ex_crypto,
      version: "0.0.1",
      name: "ExCrypto",
-     elixir: "~> 1.0",
+     elixir: ">= 1.0.0",
      deps: deps,
      docs: [extras: ["README.md"]]
    ]
@@ -18,8 +18,8 @@ defmodule ExCrypto.Mixfile do
   defp deps do
     [
       {:pipe, github: "batate/elixir-pipes"},
-      {:poison, "~> 1.5"},
-      {:timex, "~> 1.0.0-rc1"},
+      {:poison, ">= 1.0.0"},
+      {:timex, ">= 0.19.0"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.10", only: :dev}
     ]
