bc4860cf130e82040aa73587fdcc7a563fcb189b
Josh Austin <josh.austin@gmail.com>
2015-11-12 11:18:55 -0500

    add some hmac functions

---
+++ lib/ex_crypto/hmac.ex
@@ -0,0 +1,25 @@
+defmodule ExCrypto.HMAC do
+
+  def hmac(data, key) do
+    hmac(data, key, type: :sha256)
+  end
+
+  def hmac(data, key, [type: :sha256]) do
+    {:ok, :crypto.hmac(:sha256, key, data)}
+  end
+
+  def verify_hmac(data, key, other_mac) do
+    verify_hmac(data, key, other_mac, type: :sha256)
+  end
+
+  def verify_hmac(data, key, other_mac, [type: :sha256]) do
+    case hmac(data, key, type: :sha256) do
+      {:ok, my_mac} -> case my_mac === other_mac do
+        true -> {:ok, true}
+        false -> {:ok, false}
+      end
+      {:error, reason} -> {:error, reason}
+    end
+  end
+  
+end
---
+++ test/ex_crypto_hmac_test.exs
@@ -0,0 +1,51 @@
+defmodule ExCryptoHMACTest do
+  use ExUnit.Case
+
+  setup do
+    # Generate a key and data to work with
+    {:ok, aes_128_key} = ExCrypto.generate_aes_key(:aes_128, :bytes)
+    {:ok, data} = ExCrypto.rand_bytes(32)
+
+    {:ok, aes_128_key: aes_128_key, data: data}
+  end
+
+  test "generate and verify HMAC", context do
+    # Generate the MAC
+    {:ok, mac} = ExCrypto.HMAC.hmac(context[:data], context[:aes_128_key])
+    assert(mac !== nil)
+    assert(is_binary(mac))
+
+    # Check it matches the Erlang
+    check_mac = :crypto.hmac(:sha256, context[:aes_128_key], context[:data])
+    assert(check_mac === mac)
+
+    # Check it does not match when a different key is used
+    {:ok, other_aes_128_key} = ExCrypto.generate_aes_key(:aes_128, :bytes)
+    {:ok, other_mac} = ExCrypto.HMAC.hmac(context[:data], other_aes_128_key)
+    assert(mac !== other_mac)
+  end
+
+  test "hmac_verify with valid MAC", context do
+    # Generate the MAC
+    {:ok, mac} = ExCrypto.HMAC.hmac(context[:data], context[:aes_128_key])
+    assert(mac !== nil)
+    assert(is_binary(mac))
+
+    {:ok, mac_is_valid} = ExCrypto.HMAC.verify_hmac(context[:data], context[:aes_128_key], mac)
+    assert(mac_is_valid)    
+  end
+
+  test "hmac_verify with invalid MAC", context do
+    # Generate a different key
+    {:ok, other_aes_128_key} = ExCrypto.generate_aes_key(:aes_128, :bytes)
+
+    # Generate the MAC
+    {:ok, invalid_mac} = ExCrypto.HMAC.hmac(context[:data], other_aes_128_key)
+    assert(invalid_mac !== nil)
+    assert(is_binary(invalid_mac))
+
+    {:ok, mac_is_valid} = ExCrypto.HMAC.verify_hmac(context[:data], context[:aes_128_key], invalid_mac)
+    assert(mac_is_valid === false)    
+  end
+  
+end
