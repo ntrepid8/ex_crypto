52c711f92b91b3068dc67d68a16342293cc9e1b2
Josh Austin <josh.austin@gmail.com>
2015-11-12 17:09:41 -0500

    add hash! functions

--- lib/ex_crypto/hash.ex
+++ lib/ex_crypto/hash.ex
@@ -3,7 +3,21 @@ defmodule ExCrypto.Hash do
     {:ok, :crypto.hash(:sha256, data)}
   end
 
+  def sha256!(data) do
+    case sha256(data) do
+      {:ok, digest} -> digest
+      {:error, reason} -> raise "sha256 error"
+    end
+  end
+
   def sha512(data) do
     {:ok, :crypto.hash(:sha512, data)}
+  end
+
+  def sha512!(data) do
+    case sha512(data) do
+      {:ok, digest} -> digest
+      {:error, reason} -> raise "sha512 error"
+    end
   end
 end
