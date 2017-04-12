d0de2e651cb5967e114a4fc286c836e05fb6df10
Josh Austin <josh.austin@gmail.com>
2017-04-12 16:10:25 -0400

    clean up tests

--- test/ex_token_test.exs
+++ test/ex_token_test.exs
@@ -2,7 +2,7 @@ defmodule ExCrypto.ExTokenTest do
   use ExUnit.Case
   require Logger
 
-  test "Token.create/3 and Token.verify/4" do
+  test "Token.create/3 and Token.verify/4 (basic)" do
     payload = %{"foo" => "bar", "spam" => "eggs"}
     encoded_payload = Poison.encode!(payload)
     {:ok, secret} = ExCrypto.generate_aes_key(:aes_256, :bytes)
@@ -15,17 +15,13 @@ defmodule ExCrypto.ExTokenTest do
     # verify the token
     ttl = 15*60  # 15 minute TTL
     result = ExCrypto.Token.verify(token, secret, ttl)
-    Logger.debug("result=#{inspect result}")
     assert assert match?({:ok, _}, result)
   end
 
   test "Token.create/3 and Token.verify/4 (token too old)" do
     now_dt = :calendar.universal_time()
-    Logger.debug("now_dt=#{inspect now_dt}")
     now_seconds = :calendar.datetime_to_gregorian_seconds(now_dt)
-    Logger.debug("now_seconds=#{now_seconds}")
     past_seconds = now_seconds - (30*60)
-    Logger.debug("past_seconds=#{past_seconds}")
     past_dt = :calendar.gregorian_seconds_to_datetime(past_seconds)
 
     payload = %{"foo" => "bar", "spam" => "eggs"}
@@ -41,17 +37,13 @@ defmodule ExCrypto.ExTokenTest do
     # verify the token
     ttl = 15*60  # 15 minute TTL
     result = ExCrypto.Token.verify(token, secret, ttl)
-    Logger.debug("result=#{inspect result}")
     assert assert match?({:error, _}, result)
   end
 
   test "Token.create/3 and Token.verify/4 (token in future)" do
     now_dt = :calendar.universal_time()
-    Logger.debug("now_dt=#{inspect now_dt}")
     now_seconds = :calendar.datetime_to_gregorian_seconds(now_dt)
-    Logger.debug("now_seconds=#{now_seconds}")
     future_seconds = now_seconds + (30*60)
-    Logger.debug("future_seconds=#{future_seconds}")
     future_dt = :calendar.gregorian_seconds_to_datetime(future_seconds)
 
     payload = %{"foo" => "bar", "spam" => "eggs"}
@@ -67,7 +59,7 @@ defmodule ExCrypto.ExTokenTest do
     # verify the token
     ttl = 15*60  # 15 minute TTL
     result = ExCrypto.Token.verify(token, secret, ttl)
-    Logger.debug("result=#{inspect result}")
     assert assert match?({:error, _}, result)
   end
+
 end
