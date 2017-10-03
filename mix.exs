ace7bc87e12c0f2a2a212e1685f31995715d1781
Marc-Antoine ARNAUD <MarcAntoine-Arnaud@users.noreply.github.com>
2017-10-03 17:41:13 +0200

    update poison dependency (#9)

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule ExCrypto.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 2.0"},
+      {:poison, ">= 2.0.0"},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.15", only: :dev}
     ]
