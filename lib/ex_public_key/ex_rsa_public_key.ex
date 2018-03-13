3ae3e5246581e5d3fe9186281df869a22d68d4c7
Josh Austin <josh.austin@gmail.com>
2018-03-13 18:07:50 -0400

    update ExPublicKey.RSAPublicKey - add get_fingerprint/2 - add encode_der/1 - add decode_der/2

--- lib/ex_public_key/ex_rsa_public_key.ex
+++ lib/ex_public_key/ex_rsa_public_key.ex
@@ -31,4 +31,53 @@ defmodule ExPublicKey.RSAPublicKey do
         {:error, "invalid ExPublicKey.RSAPublicKey: #{rsa_public_key}"}
     end
   end
+
+  def get_fingerprint(rsa_public_key=%__MODULE__{}, opts \\ []) do
+    # parse opts
+    digest_type = Keyword.get(opts, :digest_type, :sha256)
+
+    # encode_der and hash
+    with {:ok, der_encoded} <- encode_der(rsa_public_key),
+         digest = :crypto.hash(digest_type, der_encoded),
+      do: Base.encode16(digest, case: :lower)
+  end
+
+  def encode_der(rsa_public_key=%__MODULE__{}) do
+    # hack to encode same defaults as openssl in SubjectPublicKeyInfo format
+    with {:ok, key_sequence} <- as_sequence(rsa_public_key) do
+      pem_entry = :public_key.pem_entry_encode(:SubjectPublicKeyInfo, key_sequence)
+      der_encoded =
+        :public_key.pem_encode([pem_entry])
+        |> String.trim()
+        |> String.split("\n")
+        |> Enum.filter(fn(line) -> !String.contains?(line, "-----") end)
+        |> Enum.join("")
+        |> Base.decode64!()
+      {:ok, der_encoded}
+    end
+  end
+
+  def decode_der(der_encoded, opts \\ []) do
+    # parse opts
+    format = Keyword.get(opts, :format, :SubjectPublicKeyInfo) # also supports :RSAPublicKey
+
+    # decode and parse
+    :public_key.der_decode(format, der_encoded)
+    |> from_der_encoded_0()
+  end
+
+  # Helpers
+
+  def from_der_encoded_0({:SubjectPublicKeyInfo, _, der_key}) do
+    with {:RSAPublicKey, pub_mod, pub_exp} <- :public_key.der_decode(:RSAPublicKey, der_key),
+      do: from_der_encoded_0({:RSAPublicKey, pub_mod, pub_exp})
+  end
+  def from_der_encoded_0({:RSAPublicKey, pub_mod, pub_exp}) do
+    rsa_pub_key = from_sequence({:RSAPublicKey, pub_mod, pub_exp})
+    {:ok, rsa_pub_key}
+  end
+  def from_der_encoded_0(_other) do
+    {:error, :invalid_public_key}
+  end
+
 end
